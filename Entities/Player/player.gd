0aaf2a9888dd4c4c9d4c8b9f8aaff53cc68ef247
Crimson Caelum <crimsoncaelum@proton.me>
2025-07-30 19:39:31 +0100

    Create Level 1 Scene and Add Player

---
+++ Entities/Player/player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+# Speed of the Character, When Moving
+@export var SPEED: float = 145.0
+# Acceleration of the Character, Increases the Speed with a Multiplier
+@export var ACCELERATION: float = 15.0
+
+# Animation Player
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+# Handles the Physics Process in the Game, Such as Movement Etc.
+func _process(delta: float) -> void:
+	movement()
+
+# Handles the Movement of the Player Currently Manages 4D Movement
+func movement() -> void:
+	var direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	if direction == Vector2.ZERO:
+		velocity = Vector2.ZERO
+	else:
+		velocity = direction * SPEED
+	
+	move_and_slide()
